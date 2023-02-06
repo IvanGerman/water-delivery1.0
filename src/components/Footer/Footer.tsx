34f0731bee2a19b43029e20d3ebdba2ddbd1333c
IvanGerman <ivgvi@mail.ru>
2023-02-06 17:25:23 +0700

    feat: add some components, main project structure ready

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React, { FC } from 'react';
+
+import styles from './Footer.module.scss';
+
+const Footer: FC = () => {
+  return (
+    <div className={styles.footerWrapper}>
+      Footer
+    </div>
+  );
+};
+
+export default Footer;
