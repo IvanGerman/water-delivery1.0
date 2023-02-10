58c983f5581eb135fa8e01a333a116bb3f74dfd5
IvanGerman <ivgvi@mail.ru>
2023-02-10 17:39:05 +0700

    feat: implementing swiper.js

--- src/components/FiltersPage/FiltersPage.tsx
+++ src/components/FiltersPage/FiltersPage.tsx
@@ -1,11 +1,33 @@
 import React, { FC } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
+
+import 'swiper/swiper.min.css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 import styles from './FiltersPage.module.scss';
 
 const FiltersPage: FC = () => {
   return (
     <div className={styles.filtersPageWrapper}>
-      FiltersPage
+      <Swiper
+        // install Swiper modules
+      modules={[Navigation, Pagination, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+      >
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+      </Swiper>
     </div>
   );
 };
