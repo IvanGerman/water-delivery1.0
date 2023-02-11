49d9ec09e637082dc4b69c8a565e2b14541edae1
IvanGerman <ivgvi@mail.ru>
2023-02-11 18:56:18 +0700

    feat: working on slider

--- src/components/FiltersPage/FiltersPage.tsx
+++ src/components/FiltersPage/FiltersPage.tsx
@@ -2,12 +2,12 @@ import React, { FC } from 'react';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation, Pagination, Scrollbar, A11y } from 'swiper';
 
-import 'swiper/swiper.min.css';
+import 'swiper/css';
 import 'swiper/css/navigation';
 import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
-import styles from './FiltersPage.module.scss';
+import styles from './FiltersPage.module.scss'; 
 
 const FiltersPage: FC = () => {
   return (
@@ -15,17 +15,21 @@ const FiltersPage: FC = () => {
       <Swiper
         // install Swiper modules
       modules={[Navigation, Pagination, Scrollbar, A11y]}
-      spaceBetween={50}
-      slidesPerView={3}
+      spaceBetween={20}
+      slidesPerView={1}
       navigation
       pagination={{ clickable: true }}
       scrollbar={{ draggable: true }}
       onSwiper={(swiper) => console.log(swiper)}
       onSlideChange={() => console.log('slide change')}
       >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide><div className={styles.slide1}></div></SwiperSlide>
+        <SwiperSlide><div className={styles.slide2}></div></SwiperSlide>
+        <SwiperSlide>
+          <div className={styles.slide3}>
+            <img src='./../../assets/images/spring.jpg' alt='' className={styles.slide3img} />
+          </div>
+        </SwiperSlide>
         <SwiperSlide>Slide 4</SwiperSlide>
       </Swiper>
     </div>
