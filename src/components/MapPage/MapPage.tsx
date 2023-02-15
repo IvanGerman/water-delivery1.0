e34abf10b262717806ed70a59303cf6959593f4c
IvanGerman <ivgvi@mail.ru>
2023-02-15 21:40:02 +0700

    feat: add yandex map to map page

--- src/components/MapPage/MapPage.tsx
+++ src/components/MapPage/MapPage.tsx
@@ -1,11 +1,32 @@
 import React, { FC } from 'react';
+import { YMaps, Map, Placemark } from '@pbe/react-yandex-maps';
 
 import styles from './MapPage.module.scss';
 
 const MapPage: FC = () => {
   return (
     <div className={styles.mapPageWrapper}>
-      MapPage
+      <YMaps>
+        <div>
+          My awesome application with maps!
+          <Map width={500}
+               height={500} 
+               defaultState = {
+              { center: [56.492644, 85.054446],
+               zoom: 10,
+               controls: ["zoomControl", "fullscreenControl"],
+              }}
+               modules={["control.ZoomControl", "control.FullscreenControl"]} >
+            <Placemark defaultGeometry = {[56.492644, 85.054446]} 
+                       modules={["geoObject.addon.balloon"]}
+                       properties={{
+                        balloonContentBody:
+                          "Автомат питьевой воды Родничок, ул. Павла Нарановича 10",
+                      }}
+            />
+          </Map>
+        </div>
+      </YMaps>     
     </div>
   );
 };
