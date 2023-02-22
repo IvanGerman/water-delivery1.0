bf2297e7baea0c6e44911a6eb7c7bf467bbc1cc6
IvanGerman <ivgvi@mail.ru>
2023-02-22 21:41:58 +0700

    feat: implementing another version of burger menu

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,101 +1,55 @@
-import React, { FC, useEffect, useState, useReducer  } from 'react';
-import './Header.scss';
+import React, { useState } from 'react';
+import { NavLink } from "react-router-dom";
+import { AiOutlineMenu, AiOutlineClose } from 'react-icons/ai';
 
-import Menu from '../Menu/Menu';
-
-//import styles from './Header.module.scss';
-
-
-
-const Header: FC = () => {  
-  
-  function rerender() {  console.log('rerender');
-    //toggling isActive true/false
-    setIsActive(current => !current);
-  }
-
-  let [isActive, setIsActive] = useState(false);
-
-  useEffect (() => {
-    const hamb = document.querySelector("#hamb");
-    const popup = document.querySelector("#popup");  
-    const body = document.body;
-    //@ts-ignore
-    const menu = document.querySelector("#menu").cloneNode(1);
-  
-    const renderPopup = () =>  {
-      console.log('renderPopup') 
-      popup?.appendChild(menu);
-      
-    }
-
-    //@ts-ignore
-    const links = Array.from(menu.children);
-
-    const closeOnClick = () => { console.log('closeOnClick');
-      popup?.classList.remove("open");
-      hamb?.classList.remove("active");
-      body.classList.remove("noscroll");
-      //rerender here too
-    }
-
-    links.forEach((link) => {
-      //@ts-ignore
-      link.addEventListener("click", closeOnClick);
-    });
+import style from './Header.module.scss';
+import Logo from '../../assets/images/logo.png';
+import LogoText from '../../assets/images/logo_text.png';
 
 
-
-    const hambHandler = (e: React.MouseEvent<HTMLInputElement>) => {
-      e.preventDefault();
-      console.log('hambHandler')
-      popup?.classList.toggle("open");
-      //hamb?.classList.toggle(clname);
-      body.classList.toggle("noscroll");
-      renderPopup();
-      // here we need rerender probably       
-      rerender();
-    }
-    
-    if (isActive === false) {
-      //@ts-ignore
-    hamb?.addEventListener("click", hambHandler);
-    }
-    if (isActive === true) {
-      //@ts-ignore
-    hamb?.removeEventListener("click", hambHandler);
-    }
-    
-  });
-  
-  console.log('isActive bevor render---', isActive);
-  
+const Header = () => {
+  const [nav, setNav] = useState(false);
   return (
-    <>
-      <nav className='navbar'>
+    <header className={style.header}>
       <div className='container'>
-        <div className='navbar__wrap'>
-          <div className='hamb'>
-            <div className={isActive ? 'hamb__field active' : 'hamb__field' } id="hamb">
-              <span className='bar'></span> <span className='bar'></span>
-              <span className='bar'></span>
-            </div>
+        <div className={style.box}>
+          <div className={style.logo_image}>
+            <img src={Logo} alt='/' />
+            <img src={LogoText} alt='/' />
           </div>
-          <a href="#" className='logo' id="logo">Logo</a>
-          <ul className='menu' id="menu">
-            <li><a href="#">Home</a></li>
-            <li><a href="#">Benefits</a></li>
-            <li><a href="#">Prices</a></li>
-            <li><a href="#">Blog</a></li>
-            <li><a href="#">About us</a></li>
-            <li><a href="#">Contact</a></li>
+          <ul
+            //@ts-ignore
+            className={
+              nav ? [style.menu, style.active].join(' ') : [style.menu]
+            }
+          >
+            <li>
+              <NavLink to="/">MainPage</NavLink>
+            </li>
+            <li>
+              <NavLink to="/filtersPage">FiltersPage</NavLink>
+            </li>
+            <li>
+              <NavLink to="/usePage">UsePage</NavLink>
+            </li>
+            <li>
+              <NavLink to="/mapPage">MapPage</NavLink>
+            </li>
+            <li>
+              <NavLink to="/reviewPage">ReviewPage</NavLink>
+            </li>
+            <li>
+              <NavLink to="/faqPage">FaqPage</NavLink>
+            </li>
           </ul>
+          <div onClick={() => setNav(!nav)} className={style.mobile_btn}>
+            {nav ? <AiOutlineClose size={25} /> : <AiOutlineMenu size={25} />}
+          </div>
         </div>
       </div>
-    </nav>
-    <div className='popup' id="popup"></div>
-    </>
+    </header>
   );
 };
 
 export default Header;
+ 
