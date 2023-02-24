a19e1fdebc128d04b7b3248a747772e8c473858a
IvanGerman <ivgvi@mail.ru>
2023-02-24 20:01:42 +0700

    feat: implement accordion at Faq page

--- src/components/FaqPage/FaqPage.tsx
+++ src/components/FaqPage/FaqPage.tsx
@@ -1,27 +1,124 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect } from 'react';
 
 import styles from './FaqPage.module.scss';
+import './styles.css';
 
 const FaqPage: FC = () => {
+
+  useEffect(() => {
+    const accordions = document.getElementsByClassName("accordion");
+    console.log(accordions);
+    
+  for (let i = 0; i < accordions.length; i++) {
+  accordions[i].addEventListener("click", function () {
+    //@ts-ignore
+    this.classList.toggle("active");
+    //@ts-ignore
+    let answerDiv = this.nextElementSibling;
+    if (answerDiv.style.maxHeight) {
+      answerDiv.style.maxHeight = null;
+    } else {
+      answerDiv.style.maxHeight = answerDiv.scrollHeight + "px";
+    }
+   });
+  }
+  })
+  
+
   return (
-    <div className={styles.faqPageWrapper}>
-      <h2>Часто задаваемые вопросы</h2>
-      <p>Принцип работы автомата</p>
-      <p>Как часто меняются фильтры?</p>
-      <p>По каким анализам проверяем качество воды?</p>
-      <p>Сколько стоит вода?</p>
-      <p>В какую тару можно набрать воды?</p>
-      <p>Даёт ли автомат сдачу?</p>
-      <p>В какую тару можно набрать воды?</p>
-      <p>Какие преимущества покупки воды по АКВА КЛЮЧУ?</p>
-      <p>Где можно приобрести АКВА КЛЮЧ?</p>
-      <p>Какие преимущества покупки воды по АКВА КЛЮЧУ?</p>
-      <p>Где можно приобрести АКВА КЛЮЧ?</p>
-      <p>Какие преимущества покупки воды по АКВА КЛЮЧУ?</p>
-      <p>Где можно приобрести АКВА КЛЮЧ?</p>
-      <p>Какие преимущества покупки воды по АКВА КЛЮЧУ?</p>
-      <p>Где можно приобрести АКВА КЛЮЧ?</p>
-    </div>
+    <div className="containerFluid">
+  <h2>Frequently Asked Questions(FAQs)</h2>
+  <div className="accordion">
+    <div className="icon"></div>
+    <h5>What is Lorem Ipsum?</h5>
+  </div>
+  <div className="answerDiv">
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting
+      industry. Lorem Ipsum has been the industry's standard dummy text ever
+      since the 1500s, when an unknown printer took a galley of type and
+      scrambled it to make a type specimen book. It has survived not only
+      five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged.
+    </p>
+  </div>
+
+  <div className="accordion">
+    <div className="icon"></div>
+    <h5>Why do we use it?</h5>
+  </div>
+  <div className="answerDiv">
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting
+      industry. Lorem Ipsum has been the industry's standard dummy text ever
+      since the 1500s, when an unknown printer took a galley of type and
+      scrambled it to make a type specimen book. It has survived not only
+      five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged.
+    </p>
+  </div>
+
+  <div className="accordion">
+    <div className="icon"></div>
+    <h5>Where does it come from?</h5>
+  </div>
+  <div className="answerDiv">
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting
+      industry. Lorem Ipsum has been the industry's standard dummy text ever
+      since the 1500s, when an unknown printer took a galley of type and
+      scrambled it to make a type specimen book. It has survived not only
+      five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged.
+    </p>
+  </div>
+
+  <div className="accordion">
+    <div className="icon"></div>
+    <h5>Why do we use it?</h5>
+  </div>
+  <div className="answerDiv">
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting
+      industry. Lorem Ipsum has been the industry's standard dummy text ever
+      since the 1500s, when an unknown printer took a galley of type and
+      scrambled it to make a type specimen book. It has survived not only
+      five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged.
+    </p>
+  </div>
+
+  <div className="accordion">
+    <div className="icon"></div>
+    <h5>Where does it come from?</h5>
+  </div>
+  <div className="answerDiv">
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting
+      industry. Lorem Ipsum has been the industry's standard dummy text ever
+      since the 1500s, when an unknown printer took a galley of type and
+      scrambled it to make a type specimen book. It has survived not only
+      five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged.
+    </p>
+  </div>
+
+  <div className="accordion">
+    <div className="icon"></div>
+    <h5>Where does it come from?</h5>
+  </div>
+  <div className="answerDiv">
+    <p>
+      Lorem Ipsum is simply dummy text of the printing and typesetting
+      industry. Lorem Ipsum has been the industry's standard dummy text ever
+      since the 1500s, when an unknown printer took a galley of type and
+      scrambled it to make a type specimen book. It has survived not only
+      five centuries, but also the leap into electronic typesetting,
+      remaining essentially unchanged.
+    </p>
+  </div>
+  
+</div>
   );
 };
 
