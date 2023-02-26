6d8f76a93b2102ffe3336e9f49a8a093900cfc49
IvanGerman <ivgvi@mail.ru>
2023-02-26 18:14:25 +0700

    feat: adjust the position of paragraphs on the Use Page

--- src/components/UsePage/UsePage.tsx
+++ src/components/UsePage/UsePage.tsx
@@ -25,40 +25,50 @@ const UsePage: FC = () => {
           >
             <SwiperSlide>
               <div className={styles.pWrapperDiv}>
-                <p className={styles.slider_p}>Подвесьте 5-6 л. бутыль за основание ручки,</p>
-                <p className={styles.slider_p}>бутыль большей ёмкости установите на подставку</p>
+                <div>
+                  <p className={styles.slider_p}>Подвесьте 5-6 л. бутыль за основание ручки,</p>
+                  <p className={styles.slider_p}>бутыль большей ёмкости установите на подставку</p>
+                </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className={styles.pWrapperDiv}>
-                <p className={styles.slider_p}>Приложите АКВА КЛЮЧ к считывателю или внесите аванс,</p>
-                <p className={styles.slider_p}>используя монеты или купюры</p>
-                <p className={styles.slider_p}>Автомат принимает:</p>
-                <p className={styles.slider_p}>- монеты достоинством 1, 2, 5, 10 рублей;</p>
-                <p className={styles.slider_p}>- купюры 10, 50, 100, 500, 1000 рублей.</p>
+                <div>
+                  <p className={styles.slider_p}>Приложите АКВА КЛЮЧ к считывателю или внесите аванс,</p>
+                  <p className={styles.slider_p}>используя монеты или купюры</p>
+                  <p className={styles.slider_p}>Автомат принимает:</p>
+                  <p className={styles.slider_p}>- монеты достоинством 1, 2, 5, 10 рублей;</p>
+                  <p className={styles.slider_p}>- купюры 10, 50, 100, 500, 1000 рублей.</p>
+                </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className={styles.pWrapperDiv}>
-                <p className={styles.slider_p}>Нажмите кнопку 5 или 20 литров</p>
-                <p className={styles.slider_p}>Автомат выдаёт воду порциями по 5 или 20 литров,</p>
-                <p className={styles.slider_p}>в зависимости от того, какая кнопка будет нажата.</p>
-                <p className={styles.slider_p}>Для остановки нажмите кнопку 5 или 20 литров повторно</p>
+                <div>
+                  <p className={styles.slider_p}>Нажмите кнопку 5 или 20 литров</p>
+                  <p className={styles.slider_p}>Автомат выдаёт воду порциями по 5 или 20 литров,</p>
+                  <p className={styles.slider_p}>в зависимости от того, какая кнопка будет нажата.</p>
+                  <p className={styles.slider_p}>Для остановки нажмите кнопку 5 или 20 литров повторно</p>
+                </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className={styles.pWrapperDiv}>
-                <p className={styles.slider_p}>Если Вы внесли сумму больше, чем стоимость товара,</p>
-                <p className={styles.slider_p}>который хотите приобрести, остаток средств можно</p>
-                <p className={styles.slider_p}>сохранить на АКВА КЛЮЧЕ и воспользоваться им позже</p>
+                <div>
+                  <p className={styles.slider_p}>Если Вы внесли сумму больше, чем стоимость товара,</p>
+                  <p className={styles.slider_p}>который хотите приобрести, остаток средств можно</p>
+                  <p className={styles.slider_p}>сохранить на АКВА КЛЮЧЕ и воспользоваться им позже</p>
+                </div>
               </div>
             </SwiperSlide>
             <SwiperSlide>
               <div className={styles.pWrapperDiv}>
-                <p className={styles.slider_p}>Если в течении 2 минут после внесения средств или</p>
-                <p className={styles.slider_p}>автоматического прекращения подачи воды не будет</p>
-                <p className={styles.slider_p}>нажата кнопка 5 или 20 литров,</p>
-                <p className={styles.slider_p}>внесённая сумма (остаток) аннулируется!</p>
+                <div>
+                  <p className={styles.slider_p}>Если в течении 2 минут после внесения средств или</p>
+                  <p className={styles.slider_p}>автоматического прекращения подачи воды не будет</p>
+                  <p className={styles.slider_p}>нажата кнопка 5 или 20 литров,</p>
+                  <p className={styles.slider_p}>внесённая сумма (остаток) аннулируется!</p>
+                </div>
               </div>
             </SwiperSlide>
             
